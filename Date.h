8ae581c732268f7c6602f691f0da645ea5e87421
lironsoffer <soffer.liron@gmail.com>
2016-06-18 13:34:12 +0300

    +copy constructor

--- Date.h
+++ Date.h
@@ -15,6 +15,8 @@ public:
 	Date():_day(0),_month(0),_year(0){}
 	Date(const  unsigned int day, const unsigned int month,
 			const unsigned int  year): _day(day), _month(month), _year(year){}
+	Date(const Date& orig): _day(orig._day),_month(orig._month),
+			_year(orig._year){}
 	virtual ~Date(){}
 
 	friend ostream& operator<< (ostream& os,const Date &date)
